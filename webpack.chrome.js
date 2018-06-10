7fa7d8258d92ee72a6a8b957285ef66d36744a84
Evgeniy <erizhov7@gmail.com>
2018-06-10 11:41:00 +0300

    Webpack configured for diffirent browsers building

--- webpack.chrome.js
+++ webpack.chrome.js
@@ -2,11 +2,10 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common')
 const path = require('path')
 
-process.env.CHROME = false
+process.env.CHROME = true
 
 module.exports = merge(common, {
-
     output: {
-        path: path.join(__dirname, 'dist-opera'),
+        path: path.join(__dirname, 'dist-chrome'),
     }
 })
